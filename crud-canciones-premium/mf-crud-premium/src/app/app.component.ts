c2165bbe738be53102ba073eda5643fb11e4d713
Tomás <tomas.seguram@gmail.com>
2021-11-06 15:13:30 -0600

    mf

---
+++ crud-canciones-premium/mf-crud-premium/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mf-crud-premium',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'mf-crud-premium';
+}
